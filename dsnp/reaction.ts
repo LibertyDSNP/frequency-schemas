3307e90d9d5bf068edee17982dc1c831177d129e
Wil Wade <wil.wade@unfinished.com>
2022-08-15 09:09:01 -0400

    Reaction Announcement

---
+++ dsnp/reaction.ts
@@ -0,0 +1,51 @@
+import { ParquetModel } from "../types/frequency";
+
+const reaction: ParquetModel = [
+  {
+    name: "announcementType",
+    column_type: {
+      INTEGER: {
+        bit_width: 16,
+        sign: false,
+      },
+    },
+    compression: "GZIP",
+    bloom_filter: false,
+  },
+  {
+    name: "emoji",
+    column_type: "STRING",
+    compression: "GZIP",
+    bloom_filter: true,
+  },
+  {
+    name: "apply",
+    column_type: {
+      INTEGER: {
+        bit_width: 8,
+        sign: false,
+      },
+    },
+    compression: "GZIP",
+    bloom_filter: false,
+  },
+  {
+    name: "fromId",
+    column_type: {
+      INTEGER: {
+        bit_width: 64,
+        sign: false,
+      },
+    },
+    compression: "GZIP",
+    bloom_filter: true,
+  },
+  {
+    name: "inReplyTo",
+    column_type: "STRING",
+    compression: "GZIP",
+    bloom_filter: true,
+  },
+];
+
+export default reaction;
