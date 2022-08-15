08f430af01a6f74602779753fc03af39897a17da
Wil Wade <wil.wade@unfinished.com>
2022-08-15 09:08:48 -0400

    Update Announcement

---
+++ dsnp/update.spec.ts
@@ -0,0 +1,15 @@
+import { testCompression, testParquetSchema } from "../helpers/parquet";
+import * as generators from "@dsnp/test-generators";
+import updateSchema from "./update";
+
+describe("Update Spec", () => {
+  testParquetSchema(updateSchema);
+
+  testCompression("update", updateSchema, () => ({
+    announcementType: 6,
+    contentHash: generators.generateHash(),
+    fromId: generators.randInt(10000000),
+    targetContentHash: generators.generateHash(),
+    url: `https://www.imadapp.com/data/posts/${generators.generateHash()}`,
+  }));
+});
