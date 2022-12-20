de0966b2328ad15d9a627dec8654a4c3934e4164
Wil Wade <wil.wade@unfinished.com>
2022-12-20 14:05:50 +0000

    Linting

--- read.ts
+++ read.ts
@@ -12,55 +12,60 @@ import tombstone from "./dsnp/tombstone";
 import update from "./dsnp/update";
 
 const nameAndSchema = [
-    ["dsnp.broadcast", JSON.stringify(broadcast)],
-    ["dsnp.profile", JSON.stringify(profile)],
-    ["dsnp.reaction", JSON.stringify(reaction)],
-    ["dsnp.reply", JSON.stringify(reply)],
-    ["dsnp.tombstone", JSON.stringify(tombstone)],
-    ["dsnp.update", JSON.stringify(update)],
-    ["dsnp.graphChange", JSON.stringify(graphChange)],
+  ["dsnp.broadcast", JSON.stringify(broadcast)],
+  ["dsnp.profile", JSON.stringify(profile)],
+  ["dsnp.reaction", JSON.stringify(reaction)],
+  ["dsnp.reply", JSON.stringify(reply)],
+  ["dsnp.tombstone", JSON.stringify(tombstone)],
+  ["dsnp.update", JSON.stringify(update)],
+  ["dsnp.graphChange", JSON.stringify(graphChange)],
 ];
 
 const read = async () => {
-    const api = await getFrequencyAPI();
+  const api = await getFrequencyAPI();
 
-    const { specName, specVersion } = await api.runtimeVersion;
-    const clientVersion = await api.rpc.system.version();
-    const endpointUrl = getEndpoint();
+  const { specName, specVersion } = await api.runtimeVersion;
+  const clientVersion = await api.rpc.system.version();
+  const endpointUrl = getEndpoint();
 
-    const latestBlockNumber = await api.query.system.number();
+  const latestBlockNumber = await api.query.system.number();
 
-    const connectionInfo = { endpointUrl, clientVersion, specName, specVersion, latestBlockNumber };
-    console.log("\n## Connection Information");
-    console.table(Object.entries(connectionInfo).map(([key, value]) => ({ key, value: value.toString() })));
+  const connectionInfo = { endpointUrl, clientVersion, specName, specVersion, latestBlockNumber };
+  console.log("\n## Connection Information");
+  console.table(Object.entries(connectionInfo).map(([key, value]) => ({ key, value: value.toString() })));
 
-    console.log("\n## Schema Information");
-    const maxSchemaId = await (await api.query.schemas.currentSchemaIdentifierMaximum()).toNumber();
-    console.log(`There are ${maxSchemaId} schemas on the connected chain.`);
+  console.log("\n## Schema Information");
+  const maxSchemaId = await (await api.query.schemas.currentSchemaIdentifierMaximum()).toNumber();
+  console.log(`There are ${maxSchemaId} schemas on the connected chain.`);
 
-    for (let i = 1; i <= maxSchemaId; i++) {
-        const schemaResult = await (await api.rpc.schemas.getBySchemaId(i)).unwrap();
-        const jsonSchema = Buffer.from(schemaResult.model).toString("utf8");
-        const modelParsed = JSON.parse(jsonSchema);
-        const { schema_id, model_type, payload_location } = schemaResult;
+  for (let i = 1; i <= maxSchemaId; i++) {
+    const schemaResult = await (await api.rpc.schemas.getBySchemaId(i)).unwrap();
+    const jsonSchema = Buffer.from(schemaResult.model).toString("utf8");
+    const modelParsed = JSON.parse(jsonSchema);
+    const { schema_id, model_type, payload_location } = schemaResult;
 
-        // Check for matches
-        const matchesDSNPSchemas = nameAndSchema.reduce((arr, [n, s]) => {
-            if (s === jsonSchema) arr.push(n);
-            return arr;
-        }, [])
+    // Check for matches
+    const matchesDSNPSchemas = nameAndSchema.reduce((arr, [schemaName, schemaString]) => {
+      if (schemaString === jsonSchema) arr.push(schemaName);
+      return arr;
+    }, []);
 
-        console.log(`\n## Schema Id ${schema_id}`);
-        console.table(Object.entries({
-            schema_id, model_type, payload_location, matchesDSNPSchemas
-        }).map(([key, value]) => ({ key, value: value.toString() })));
-        console.log("\n## Schema Model");
-        console.log(stringify(modelParsed));
-    }
-}
+    console.log(`\n## Schema Id ${schema_id}`);
+    console.table(
+      Object.entries({
+        schema_id,
+        model_type,
+        payload_location,
+        matchesDSNPSchemas,
+      }).map(([key, value]) => ({ key, value: value.toString() }))
+    );
+    console.log("\n## Schema Model");
+    console.log(stringify(modelParsed));
+  }
+};
 
 export const main = async () => {
-    await read();
+  await read();
 };
 
 main().catch(console.error).finally(process.exit);
