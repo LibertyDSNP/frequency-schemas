cd37af2bdb70d5537f09276cacf315e1dfff4b5e
Wil Wade <wil.wade@unfinished.com>
2022-12-20 13:56:58 +0000

    New npm run read tool

--- README.md
+++ README.md
@@ -26,3 +26,70 @@ To register a single schema:
 e.g. To register the "profile" schema
 
     npm run deploy profile
+
+# Additional Tools
+
+## Read all Schemas from a Chain
+
+```
+DEPLOY_SCHEMA_ENDPOINT_URL="ws://127.0.0.1:9944" npm run read
+```
+
+Will output various information about the schemas on the chain as well as attempt to match known DSNP schemas.
+
+### Example Output
+
+```
+## Connection Information
+┌─────────┬─────────────────────┬────────────────────────────────────────────┐
+│ (index) │         key         │                   value                    │
+├─────────┼─────────────────────┼────────────────────────────────────────────┤
+│    0    │    'endpointUrl'    │ 'wss://frequency-seal.liberti.social:9944' │
+│    1    │   'clientVersion'   │            '0.1.0-377bbe37fbe'             │
+│    2    │     'specName'      │             'frequency-rococo'             │
+│    3    │    'specVersion'    │                    '1'                     │
+│    4    │ 'latestBlockNumber' │                    '16'                    │
+└─────────┴─────────────────────┴────────────────────────────────────────────┘
+
+## Schema Information
+There are 8 schemas on the connected chain.
+
+## Schema Id 1
+┌─────────┬──────────────────────┬───────────────────────────────┐
+│ (index) │         key          │             value             │
+├─────────┼──────────────────────┼───────────────────────────────┤
+│    0    │     'schema_id'      │              '1'              │
+│    1    │     'model_type'     │           'Parquet'           │
+│    2    │  'payload_location'  │            'IPFS'             │
+│    3    │ 'matchesDSNPSchemas' │ 'dsnp.broadcast,dsnp.profile' │
+└─────────┴──────────────────────┴───────────────────────────────┘
+
+## Schema Model
+[
+  {
+    "name": "announcementType",
+    "column_type": {"INTEGER": {"bit_width": 32, "sign": true}},
+    "compression": "GZIP",
+    "bloom_filter": false
+  },
+  {
+    "name": "contentHash",
+    "column_type": "BYTE_ARRAY",
+    "compression": "GZIP",
+    "bloom_filter": true
+  },
+  {
+    "name": "fromId",
+    "column_type": {"INTEGER": {"bit_width": 64, "sign": false}},
+    "compression": "GZIP",
+    "bloom_filter": true
+  },
+  {
+    "name": "url",
+    "column_type": "STRING",
+    "compression": "GZIP",
+    "bloom_filter": false
+  }
+]
+...
+```
---
+++ read.ts
@@ -0,0 +1,66 @@
+import "@frequency-chain/api-augment";
+import { getEndpoint, getFrequencyAPI } from "./services/connect";
+
+import stringify from "json-stringify-pretty-compact";
+
+import broadcast from "./dsnp/broadcast";
+import graphChange from "./dsnp/graphChange";
+import profile from "./dsnp/profile";
+import reaction from "./dsnp/reaction";
+import reply from "./dsnp/reply";
+import tombstone from "./dsnp/tombstone";
+import update from "./dsnp/update";
+
+const nameAndSchema = [
+    ["dsnp.broadcast", JSON.stringify(broadcast)],
+    ["dsnp.profile", JSON.stringify(profile)],
+    ["dsnp.reaction", JSON.stringify(reaction)],
+    ["dsnp.reply", JSON.stringify(reply)],
+    ["dsnp.tombstone", JSON.stringify(tombstone)],
+    ["dsnp.update", JSON.stringify(update)],
+    ["dsnp.graphChange", JSON.stringify(graphChange)],
+];
+
+const read = async () => {
+    const api = await getFrequencyAPI();
+
+    const { specName, specVersion } = await api.runtimeVersion;
+    const clientVersion = await api.rpc.system.version();
+    const endpointUrl = getEndpoint();
+
+    const latestBlockNumber = await api.query.system.number();
+
+    const connectionInfo = { endpointUrl, clientVersion, specName, specVersion, latestBlockNumber };
+    console.log("\n## Connection Information");
+    console.table(Object.entries(connectionInfo).map(([key, value]) => ({ key, value: value.toString() })));
+
+    console.log("\n## Schema Information");
+    const maxSchemaId = await (await api.query.schemas.currentSchemaIdentifierMaximum()).toNumber();
+    console.log(`There are ${maxSchemaId} schemas on the connected chain.`);
+
+    for (let i = 1; i <= maxSchemaId; i++) {
+        const schemaResult = await (await api.rpc.schemas.getBySchemaId(i)).unwrap();
+        const jsonSchema = Buffer.from(schemaResult.model).toString("utf8");
+        const modelParsed = JSON.parse(jsonSchema);
+        const { schema_id, model_type, payload_location } = schemaResult;
+
+        // Check for matches
+        const matchesDSNPSchemas = nameAndSchema.reduce((arr, [n, s]) => {
+            if (s === jsonSchema) arr.push(n);
+            return arr;
+        }, [])
+
+        console.log(`\n## Schema Id ${schema_id}`);
+        console.table(Object.entries({
+            schema_id, model_type, payload_location, matchesDSNPSchemas
+        }).map(([key, value]) => ({ key, value: value.toString() })));
+        console.log("\n## Schema Model");
+        console.log(stringify(modelParsed));
+    }
+}
+
+export const main = async () => {
+    await read();
+};
+
+main().catch(console.error).finally(process.exit);
