36b5648efb1cc98409a10eee00976b9652555be3
Wil Wade <wil.wade@unfinished.com>
2022-12-20 13:56:35 +0000

    Expose access to the endpoint used

--- services/connect.ts
+++ services/connect.ts
@@ -5,13 +5,8 @@ import { KeyringPair } from "@polkadot/keyring/types";
 
 // DEPLOY_SCHEMA_ENDPOINT_URL (environment variable)
 // The value is a URL for the RPC endpoint.  e.g. ws://localhost:9944
-export async function getFrequencyAPI(): Promise<ApiPromise> {
-  let DEPLOY_SCHEMA_ENDPOINT_URL = process.env.DEPLOY_SCHEMA_ENDPOINT_URL;
-  if (DEPLOY_SCHEMA_ENDPOINT_URL === undefined) {
-    // One would think that localhost would also work here but it doesn't consistently.
-    DEPLOY_SCHEMA_ENDPOINT_URL = "ws://127.0.0.1:9944";
-  }
-  const DefaultWsProvider = new WsProvider(DEPLOY_SCHEMA_ENDPOINT_URL);
+export async function getFrequencyAPI() {
+  const DefaultWsProvider = new WsProvider(getEndpoint());
 
   // The "options" parameter pulls in the Frequency API extrinsics
   const api = await ApiPromise.create({
@@ -20,6 +15,15 @@ export async function getFrequencyAPI(): Promise<ApiPromise> {
   });
   await api.isReady;
   return api;
+}
+
+export function getEndpoint() {
+  let DEPLOY_SCHEMA_ENDPOINT_URL = process.env.DEPLOY_SCHEMA_ENDPOINT_URL;
+  if (DEPLOY_SCHEMA_ENDPOINT_URL === undefined) {
+    // One would think that localhost would also work here but it doesn't consistently.
+    DEPLOY_SCHEMA_ENDPOINT_URL = "ws://127.0.0.1:9944";
+  }
+  return DEPLOY_SCHEMA_ENDPOINT_URL;
 }
 
 // DEPLOY_SCHEMA_ACCOUNT_URI (environment variable)
