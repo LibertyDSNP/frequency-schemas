51fa35878eb4b395f908c94ef946ee6a904dde19
Wil Wade <wil.wade@unfinished.com>
2023-01-09 10:46:26 -0500

    Update for DIP-216 part of DSNP 1.2.0

--- dsnp/update.spec.ts
+++ dsnp/update.spec.ts
@@ -9,6 +9,7 @@ describe("Update Spec", () => {
     announcementType: 6,
     contentHash: generators.generateHash(),
     fromId: generators.randInt(10000000),
+    targetAnnouncementType: 2,
     targetContentHash: generators.generateHash(),
     url: `https://www.imadapp.com/data/posts/${generators.generateHash()}`,
   }));
